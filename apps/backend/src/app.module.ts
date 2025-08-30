d17630e25fb8bec1c3a556c09f075e9ccbd34b32
Владислав Трегубов <valdiszz53@gmail.com>
2025-08-30 17:31:11 +0300

    Создал модуль для сущности "notes"

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { NotesModule } from './notes/notes.module'
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
+    imports: [NotesModule],
 })
 export class AppModule {}
